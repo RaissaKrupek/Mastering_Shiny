d9925c1f80d320373f4799229f9870919cd7e5fc
Raissa Souza Krupek <raissa.krupek@usp.br>
2023-12-22 12:11:48 -0300

    Update Chapter_2_BasicUI.R

--- Chapter_2_BasicUI.R
+++ Chapter_2_BasicUI.R
@@ -6,21 +6,21 @@ ui <- fluidPage(
 server<- function (input, output, session){}
 shinyApp(ui, server)
 
-#Free Text
+#FREE TEXT
 textInput("name", "What's your name?"),
 passwordInput("password", "What's your password?"),
 textAreaInput("story", "Tell me about yourself", rows = 3)
 
-#Numeric Imputs
+#NUMERIC INPUTS
 numericInput("num", "Number one", value = 0, min = 0, max = 100),
 sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
 sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100)
 
-#Dates
+#DATES
 dateInput("dob", "When were you born?"), #dob = ID para date of birth
 dateRangeInput("holiday", "When do you want to go on vacation next?")
 
-#Limited Choices 
+#LIMITED CHOICES
 selectInput("state", "What's your favourite state?", state.name),
 radioButtons("animal", "What's your favourite animal?", animals)
 #---------------------------------------------------------------
@@ -41,10 +41,10 @@ checkboxGroupInput("states", "What states do you like?", state.name)
 checkboxInput("cleanup", "Clean up?", value = FALSE),
 checkboxInput("shutdown", "Shutdown?")
 
-#File Uploads
+#FILE UPLOADS
 fileInput("upload", NULL)
 
-#Action Buttons
+#ACTION BUTTONS
 actionButton("click", "Click me!"),
 actionButton("drink", "Drink me!", icon = icon("cocktail"))
 ##To costumize the appearance: "btn-primary", "btn-success", "btn-info", "btn-warning", or "btn-danger".
@@ -52,4 +52,38 @@ actionButton("drink", "Drink me!", icon = icon("cocktail"))
 ##To span the entire width of the element "btn-block"
 
 
-########################OUTPUTS#############################
+########################OUTPUTS##############################
+
+#TEXT
+ui <- fluidPage(
+  textOutput("text"),
+  verbatimTextOutput("code")
+)
+server <- function(input, output, session) {
+  output$text <- renderText({
+    "Hello friend!"
+  })
+  output$code <- renderPrint({
+    summary(1:10)
+  })
+}
+shinyApp(ui, server)
+
+##with {} omitted
+server <- function(input, output, session) {
+  output$text <- renderText("Hello friend!")
+  output$code <- renderPrint(summary(1:10))
+}
+
+#TABLES
+ui <- fluidPage(
+  tableOutput("static"),
+  dataTableOutput("dynamic")
+)
+server <- function(input, output, session) {
+  output$static <- renderTable(head(mtcars))
+  output$dynamic <- renderDataTable(mtcars, options = list(pageLength = 5))
+}
+shinyApp(ui, server)
+
+#PLOTS
