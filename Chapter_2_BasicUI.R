9caa63f3352360215d3559d56ed28856651f199c
Raissa Souza Krupek <raissa.krupek@usp.br>
2023-12-22 12:18:53 -0300

    Update Chapter_2_BasicUI.R

--- Chapter_2_BasicUI.R
+++ Chapter_2_BasicUI.R
@@ -87,3 +87,10 @@ server <- function(input, output, session) {
 shinyApp(ui, server)
 
 #PLOTS
+ui <- fluidPage(
+  plotOutput("plot", width = "400px")
+)
+server <- function(input, output, session) {
+  output$plot <- renderPlot(plot(1:5), res = 96)
+}
+shinyApp(ui, server)
