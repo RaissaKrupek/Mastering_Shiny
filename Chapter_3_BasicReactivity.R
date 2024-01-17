001a4e08334afadff3250f350f598746b66f3b2c
Raissa Souza Krupek <raissa.krupek@usp.br>
2024-01-16 21:32:00 -0300

    Update Chapter_3_BasicReactivity.R

--- Chapter_3_BasicReactivity.R
+++ Chapter_3_BasicReactivity.R
@@ -37,3 +37,89 @@ server <- function(input, output, session) {
   })
 }
 shinyApp(ui, server)
+
+# Timed Invalidation
+ui <- fluidPage(
+  fluidRow(
+    column(3,
+           numericInput("lambda1", label = "lambda1", value = 3),
+           numericInput("lambda2", label = "lambda2", value = 5),
+           numericInput("n", label = "n", value = 1e4, min = 0)
+    ),
+    column(9, plotOutput("hist"))
+  )
+)
+server <- function(input, output, session) {
+  timer <- reactiveTimer(500)
+  x1 <- reactive({timer()
+    rpois(input$n, input$lambda1)
+  })
+  x2 <- reactive({
+    timer()
+    rpois(input$n, input$lambda2)
+  })
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+  }, res = 96)
+}
+
+# On Click
+ui <- fluidPage(
+  fluidRow(
+    column(3,
+           numericInput("lambda1", label = "lambda1", value = 3),
+           numericInput("lambda2", label = "lambda2", value = 5),
+           numericInput("n", label = "n", value = 1e4, min = 0),
+           actionButton("simulate", "Simulate!")
+    ),
+    column(9, plotOutput("hist"))
+  )
+)
+
+##This next server doesn’t achieve our goal because it just introduces a new dependency: x1() and x2() will update
+##when we click the simulate button, but they’ll also continue to update when lambda1,
+##lambda2, or n change.
+
+server <- function(input, output, session) {
+  x1 <- reactive({
+    input$simulate
+    rpois(input$n, input$lambda1)
+  })
+  x2 <- reactive({
+    input$simulate
+    rpois(input$n, input$lambda2)
+  })
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+  }, res = 96)
+}
+
+##This is a way to use input values without taking a
+##reactive dependency on them. We need eventReactive(), which has two arguments:
+##the first argument specifies what to take a dependency on, and the second argument
+##specifies what to compute. That allows this app to only compute x1() and x2() when
+##simulate is clicked
+server <- function(input, output, session) {
+  x1 <- eventReactive(input$simulate, {
+    rpois(input$n, input$lambda1)
+  })
+  x2 <- eventReactive(input$simulate, {
+    rpois(input$n, input$lambda2)
+  })
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+  }, res = 96)
+}
+
+# Observers
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  output$greeting <- renderText(string())
+  observeEvent(input$name, {
+    message("Greeting performed")
+  })
+}
